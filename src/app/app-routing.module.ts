e77965a0804f963d0b51dc8ae2a8afb52c5dd0f5
Jhzeg96 <jhzeg96@gmail.com>
2021-04-01 11:10:02 -0600

    Se agrego la ruta enmidirectorio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AppComponent} from './app.component';
 import { BPrincipalComponent } from './componentes/bolsa-de-trabajo/b-principal/b-principal.component'
-
+import { EPrincipalComponent } from './componentes/enmidirectorio/e-principal/e-principal.component';
 const routes: Routes = [
-  {path: 'bolsa-de-trabajo', component: BPrincipalComponent}
+  {path: 'bolsa-de-trabajo', component: BPrincipalComponent},
+  {path: 'enmidirectorio', component: EPrincipalComponent},
 ];
 
 @NgModule({
