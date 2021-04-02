a68fbac861658c00ca88e6d4ab95c1704a71dddb
RecGame <morenob2525@gmail.com>
2021-04-02 12:43:15 -0600

    se agrego y creo componente de mi directorio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import {AppComponent} from './app.component';
 import { BPrincipalComponent } from './componentes/bolsa-de-trabajo/b-principal/b-principal.component'
 import {InformacionOfertaComponent} from './Componentes/informacionOferta/informacion-oferta/informacion-oferta.component'
+import { EPrincipalComponent } from './Componentes/enmidirectorio/e-principal/e-principal.component'
 
 const routes: Routes = [
   {path: '', component: BPrincipalComponent},
-  {path: 'informacion-oferta', component: InformacionOfertaComponent}
+  {path: 'informacion-oferta', component: InformacionOfertaComponent},
+  {path: 'Directorio', component: EPrincipalComponent}
 ];
 
 @NgModule({
