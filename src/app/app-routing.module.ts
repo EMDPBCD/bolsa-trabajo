40751ebff769353129251604a50ac926cdb7c107
RecGame <bjmr.2525@gmail.com>
2021-04-17 11:57:02 -0500

    creacion de noticias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { BPrincipalComponent } from './componentes/bolsa-de-trabajo/b-principal/
 import { InformacionOfertaComponent} from './componentes/bolsa-de-trabajo/informacion-oferta/informacion-oferta.component'
 import { EPrincipalComponent } from './componentes/enmidirectorio/e-principal/e-principal.component';
 import { EEmpresaInformacionComponent } from './componentes/enmidirectorio/e-empresa-informacion/e-empresa-informacion.component';
+import { NPrincipalComponent } from './componentes/noticiero/n-principal/n-principal/n-principal.component';
 
 const routes: Routes = [
   {path: 'bolsa-de-trabajo', component: BPrincipalComponent},
   {path: 'informacion-oferta/:oferta', component: InformacionOfertaComponent},
   {path: 'enmidirectorio', component: EPrincipalComponent},
-  {path: 'enmidirectorio/informacion/empresa/:id', component: EEmpresaInformacionComponent}
+  {path: 'enmidirectorio/informacion/empresa/:id', component: EEmpresaInformacionComponent},
+  {path: 'noticias', component: NPrincipalComponent}
 ];
 
 @NgModule({
