4e31c3a6f93299cf5226e1e89a86f35d016f86b4
Jhzeg96 <jhzeg96@gmail.com>
2021-04-23 15:48:55 -0500

    Se agrego el componente de noticias y se conecto con la api

---
+++ src/app/componentes/noticias/principal/noticias.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-noticias',
+  templateUrl: './noticias.component.html',
+  styleUrls: ['./noticias.component.css']
+})
+export class NoticiasPrincipalComponent implements OnInit {
+  noticias = [];
+
+  constructor(private http: HttpClient, private route: ActivatedRoute,
+    private router: Router) { }
+
+  ngOnInit() 
+  {
+      this.noticias = new Array();
+      this.http
+        .get<any>
+        (
+          'http://localhost:80/hoy-en-laredo/src/api.php/noticias/principal'
+        )
+        .subscribe((data) => 
+        {
+          for (let i = 0; i < data.length; i++) {
+            this.noticias.push(data[i]);
+          }
+          //alert(JSON.stringify(data));
+        });
+  }
+
+  verMas(id) {
+    this.router.navigate(['informacion-noticia', id], 
+    {
+      relativeTo: this.route,
+    });
+  }
+
+}
